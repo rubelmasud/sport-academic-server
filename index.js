219c0b8b515c117b4bfa47f38a0e22c578613661
MD:Rubrl RM <inforubel251@gmail.com>
2023-06-09 18:15:03 +0600

    add to jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
@@ -7,6 +8,21 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'Unauthorized Access' })
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(401).send({ error: true, message: 'Unauthorized Access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oikc1wt.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,25 +42,47 @@ async function run() {
         await client.connect();
 
         const usersCollection = client.db("sportDB").collection('users')
+        const classesCollection = client.db('sportDB').collection("allClass")
 
 
-        // user related api
+        // JWT
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+        // admin related api
+        // user collection
+        app.get('/user/:email', async (req, res) => {
+            const email = req.params.email
+            console.log(email);
+        })
+
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result)
         })
         // users api to save email & role
-        app.put('/users/:email', async (req, res) => {
-            const email = req.params.email
+        app.post('/users', async (req, res) => {
             const user = req.body;
-            const query = { email: email }
-            const options = { upsert: true }
-            const updateDog = {
-                $set: user
+            const query = { email: user.email }
+            const exitingUser = await usersCollection.findOne(query)
+            if (exitingUser) {
+                return res.send({ message: "Is User Already Exist" })
             }
-            const result = await usersCollection.updateOne(query, updateDog, options)
+            const result = await usersCollection.insertOne(user);
             res.send(result)
         })
+
+
+        // app.get('/users/role/:email', verifyJWT, async (req, res) => {
+        //     const email = res.params.email;
+        //     const query = { email: email };
+        //     const user = await usersCollection.findOne(query)
+        //     const result = { admin: user?.role === 'admin' }
+        //     res.send(result)
+        // })
 
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id
@@ -72,7 +110,11 @@ async function run() {
             res.send(result)
         })
 
-
+        // class collection
+        app.get('/allclass', async (req, res) => {
+            const result = await classesCollection.find().toArray();
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
