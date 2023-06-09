d64610a43b258b368a9f3d4f7ddf3ebbf115d0a8
MD:Rubrl RM <inforubel251@gmail.com>
2023-06-09 19:51:04 +0600

    add to vercel json file and deploy

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
 
         const usersCollection = client.db("sportDB").collection('users')
         const classesCollection = client.db('sportDB').collection("allClass")
